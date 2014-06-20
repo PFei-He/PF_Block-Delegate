5e940c932397ff68a8074315dce86474216092a8
PFei_He <user@userdeMac-mini-2.local>
2014-06-20 11:55:53 +0800

    Initial Commit

---
+++ PF_Block&Delegate/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PF_Block&Delegate
+//
+//  Created by PFei_He on 14-6-20.
+//  Copyright (c) 2014年 PFei_He. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
